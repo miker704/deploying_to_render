fa337b156c083dd58f02aa2b626f7df8ddbfc69e
miker704 <miker704@gmail.com>
2022-11-18 13:09:22 -0500

    shell edit

---
+++ shell files/rails_version_6.XX.7.X.X.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+npm install
+npm run build
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+rails db:seed #if needed
